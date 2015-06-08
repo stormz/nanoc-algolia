dddae8e74a471c75ed55b396fe4ee7c4303e3c53
François de Metz <francois@2metz.fr>
2015-06-08 18:02:26 +0200

    Remove last index.

--- lib/nanoc-algolia.rb
+++ lib/nanoc-algolia.rb
@@ -14,10 +14,6 @@ class SearchFilter < Nanoc::Filter
     raise ArgumentError.new 'Missing algolia:api_key' unless @config[:algolia][:api_key]
     raise ArgumentError.new 'Missing algolia:index' unless @config[:algolia][:index]
 
-    @last_indexed_file = '.nanoc_algolia'
-
-    load_last_timestamp
-
     Algolia.init application_id: @config[:algolia][:application_id],
                  api_key: @config[:algolia][:api_key]
 
@@ -28,11 +24,6 @@ class SearchFilter < Nanoc::Filter
   # The main content from each page is extracted and indexed at algolia
   # The id of each document will be the absolute url to the resource without domain name
   def run(content, params={})
-    # only process item that are changed since last regeneration
-    if (!@last_indexed.nil? && @last_indexed > item.mtime)
-      return content
-    end
-
     puts "Indexing page #{@item.identifier}"
 
     page_text = extract_text(content)
@@ -44,30 +35,11 @@ class SearchFilter < Nanoc::Filter
       }, item.identifier)
     puts 'Indexed ' << item.identifier
 
-    @last_indexed = Time.now
-    write_last_indexed
-
     content
   end
 
   def extract_text(content)
     doc = Nokogiri::HTML(content)
     doc.xpath('//*/text()').to_a.join(" ").gsub("\r"," ").gsub("\n"," ")
-  end
-
-  def write_last_indexed
-    begin
-      File.open(@last_indexed_file, 'w') {|f| Marshal.dump(@last_indexed, f)}
-    rescue
-      puts 'WARNING: cannot write indexed timestamps file.'
-    end
-  end
-
-  def load_last_timestamp
-    begin
-      @last_indexed = File.open(@last_indexed_file, "rb") {|f| Marshal.load(f)}
-    rescue
-      @last_indexed = nil
-    end
   end
 end
